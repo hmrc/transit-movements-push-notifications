e88c4946ae258ed4b6e9a7809b668d13eb06897c
joecefnhedog <joseph.bowen@digital.hmrc.gov.uk>
2024-09-02 15:42:15 +0100

    CTCP-5833: platop comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,10 +4,11 @@ object AppDependencies {
 
   private val bootstrapPlayVersion = "9.4.0"
   private val catsVersion          = "2.6.1"
+  private val mongoPlay = "2.2.0"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30"    % bootstrapPlayVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"           % "2.2.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"           % mongoPlay,
     "io.lemonlabs"      %% "scala-uri"                    % "4.0.2",
     "org.typelevel"     %% "cats-core"                    % catsVersion,
     "uk.gov.hmrc"       %% "internal-auth-client-play-30" % "3.0.0"
@@ -16,7 +17,7 @@ object AppDependencies {
   val test = Seq(
     "org.apache.pekko" %% "pekko-testkit" % "1.0.3",
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapPlayVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % "1.4.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % mongoPlay,
     "org.typelevel"     %% "cats-core"               % catsVersion,
     "org.apache.pekko"  %% "pekko-testkit"           % "1.0.2",
     "org.mockito"       %% "mockito-scala-scalatest" % "1.17.14",
